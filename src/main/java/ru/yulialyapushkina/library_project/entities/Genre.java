79bbc10aaa64179b907575d2a631a51612713739
Julia1284 <julia123456-84@mail.ru>
2024-07-26 14:35:07 +0300

    created entities book, author, genre

---
+++ src/main/java/ru/yulialyapushkina/library_project/entities/Genre.java
@@ -0,0 +1,23 @@
+package ru.yulialyapushkina.library_project.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String name;
+
+}
